4508a931957225b65851801172dce6e3476caaee
MathBarbos <matheusbarbosa2099@gmail.com>
2024-03-20 18:15:08 -0300

    create header and presentention banner

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,34 @@
     <v-navigation-drawer app v-model="drawer" :mini-variant="mini" permanent>
 
     </v-navigation-drawer>
-    <v-app-bar>
+    <v-app-bar color="transparent"  :elevation="0">
       <img src="images/logo-sirius.png" alt="sirius logo" width="10%">
       <v-spacer></v-spacer>
-      <v-tabs v-model="tab" density="comfortable">
-        <v-tab :hide-slider="true" value="home"><h4>Home</h4></v-tab>
-        <v-tab :hide-slider="true" value="about"><h4>Sobre nós</h4></v-tab>
-        <v-tab :hide-slider="true" value="portfolio"><h4>Produtos</h4></v-tab>
-        <v-tab :hide-slider="true" value="contact"><h4>Contato</h4></v-tab>
+      <v-tabs class="text-indigo-darken-4" v-model="tab" density="comfortable">
+        <v-tab :hide-slider="true" value="home">
+          <h4>Home</h4>
+        </v-tab>
+        <v-tab :hide-slider="true" value="about">
+          <h4>Sobre nós</h4>
+        </v-tab>
+        <v-tab :hide-slider="true" value="portfolio">
+          <h4>Produtos</h4>
+        </v-tab>
+        <v-tab :hide-slider="true" value="contact">
+          <h4>Contato</h4>
+        </v-tab>
       </v-tabs>
     </v-app-bar>
+    <v-parallax class="gradient" src="/images/home.png" style="height: 25em;">
+      <div class="d-flex flex-column fill-height justify-center align-center text-indigo-darken-4">
+        <h1 class="text-h3 font-weight-thin mb-4">
+          Qualidade e excelência em cada detalhe.
+        </h1>
+      </div>
+    </v-parallax>
     <v-main class="bg-indigo-darken-4">
-      <v-container fluid>
-        <router-view></router-view>
-      </v-container>
+      <router-view></router-view>
     </v-main>
-
     <v-footer app>
 
     </v-footer>
@@ -45,6 +57,12 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.header-main {
+  background-image: linear-gradient(45deg, #ff0 0%, #f0f 100%);
+}
+.gradient {
+  -webkit-mask-image: linear-gradient(to bottom, transparent 4%,white 40%);
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,5 @@
 <template>
     <div>
-        <v-card variant="tonal" flat image="images/home.png" height="100">
-            <v-card-title class="text-indigo-darken-4 font-weight-bold bg-white">Qualidade e excelência em cada detalhe</v-card-title>
-        </v-card>
     </div>
 </template>
 
@@ -12,8 +9,4 @@ export default {
 }
 </script>
 
-<style>
-    .slogan {
-        background-image: url("/public/images/home.png");
-    }
-</style>
+<style></style>
