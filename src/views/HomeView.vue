b7286e7fa7d9996a70e2e35809d515022b6719b8
MathBarbos <matheusbarbosa2099@gmail.com>
2024-03-22 00:09:32 -0300

    add corpo home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,24 @@
 <template>
     <div>
+        <v-container fluid style="margin-top: 5px;">
+        <v-row><v-col>
+                <v-card variant="tonal" style="padding: 15px;"> A <strong class="text-indigo-darken-4">Sirius Marmoraria</strong> se destaca por sua missão de transformar a experiência da obra com amor e
+                    excelência. Nossa prioridade é oferecer atendimento perfeito desde o início até a entrega final.
+                    Valores
+                    como excelência em atendimento, acabamento impecável e comprometimento com seu projeto são
+                    fundamentais para
+                    nós. <strong class="text-indigo-darken-4">Conte conosco para tornar seu projeto em algo extraordinário.</strong></v-card>
+            </v-col>
+            <v-col>
+                <p>A Sirius Marmoraria se destaca por sua missão de transformar a experiência da obra com amor e
+                    excelência. Nossa prioridade é oferecer atendimento perfeito desde o início até a entrega final.
+                    Valores
+                    como excelência em atendimento, acabamento impecável e comprometimento com seu projeto são
+                    fundamentais para
+                    nós. Conte conosco para tornar seu projeto em algo extraordinário.</p>
+            </v-col>
+        </v-row>
+    </v-container>
     </div>
 </template>
 
